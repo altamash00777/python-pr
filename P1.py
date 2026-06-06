9717aca5d2254c7c1b9dd8a7fcf411bd9cb1c297
Altamash <altamasn186@gmail.com>
2026-06-06 08:58:08 +0530

    python begins

---
+++ P1.py
@@ -0,0 +1,53 @@
+# print("hello world")
+
+# x="altamash"
+# print("my name is ",x)
+
+# name="altamash"
+# age=20
+# cgpa=8.4
+
+# print(name)
+# print(age)
+# print(cgpa)
+
+# name=input("enter your name:")
+# print("hello ",name)
+
+# num1=int(input("enter 1st num"))
+# num2=int(input("enter 2nd num"))
+
+# sum=num1+num2
+
+# print("sum is ",sum)
+
+# age=20
+
+# if age>=18:
+#     print("you can vote")
+# else:
+#     print("you cannot vote")
+
+
+# even odd
+
+# num=int(input("enter number: "))
+
+# if num%2==0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+# multiple if else
+
+# marks=int(input("enter marks :"))
+
+# if marks>=90:
+#     print("topper")
+# elif marks>=70:
+#     print("topper kam")
+# elif marks>=50:
+#     print("topper kam kam")   
+
+# else:
+#     print("not topper")
