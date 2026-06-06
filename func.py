e5d008699b270b8c2ddfb6aba310ff22b9c3a7e9
Altamash <altamasn186@gmail.com>
2026-06-06 17:08:28 +0530

    changes in func.py

--- func.py
+++ func.py
@@ -61,9 +61,43 @@
 # greet() 
 
 
+# def intro(name,age):
+#     print("name is ",name ,"age is ",age)
+
+# intro(name="altamash" , age=32)
+
+num1=int(input("enter number 1st : "))
+num2=int(input("enter number 2nd : "))
+
+def add(num1,num2):
+   return num1+num2
+
+def sub(num1,num2):
+   return num1-num2
+
+def multi(num1,num2):
+   return num1*num2
+
+def div(num1,num2):
+   return num1/num2
 
 
+print("choose operation")
+print("1 Add")
+print("2 Sub")
+print("3 Multi")
+print("4 Div")
 
+choice = int(input("enter choice"))
 
-
+if choice == 1:
+   print("sum is ",add(num1,num2))
+elif choice == 2:
+   print("sub is ",sub(num1,num2))
+elif choice == 3:
+   print("multiply is ",multi(num1,num2))
+elif choice == 4:
+   print("div is ",div(num1,num2))
+else:
+   print("invalid")
 
