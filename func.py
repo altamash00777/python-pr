05839e472ab89984fc2f732c802ba2eaf27491d6
Altamash <altamasn186@gmail.com>
2026-06-06 09:33:22 +0530

    function

---
+++ func.py
@@ -0,0 +1,69 @@
+# function is block of code that help to write specific task 
+# single time and later we can use them again
+
+# def welcome():
+#     print("function")
+
+# welcome()
+# welcome()
+# welcome()
+# welcome()
+
+
+# def greet():
+#     print("hello altamahs")
+#     print("what going in your life")
+
+# greet()
+# greet()
+
+# function with parameters
+
+# def student(name):
+#     print("name is ",name)
+
+# student("altamash")
+
+
+# def student(name,age):
+#     print("name-> ",name)
+#     print("age-> ",age)
+
+# student("altamash",20)
+
+# return keyword use to send value
+# back to
+
+# def sum(a,b):
+#     return a+b
+
+# result=sum(20,30)
+
+# print(result)
+
+
+# def sub(a,b):
+#     return a-b
+
+# result=sub(23,33)
+
+# print(result)
+
+# default parameter
+# in default parameter we pass value inside fun 
+# by default if we not pass any value
+# it will return that
+
+# def greet(name="guest"):
+#     print("name is ",name)
+
+# greet("altamash")
+# greet() 
+
+
+
+
+
+
+
+
