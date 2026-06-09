ee51ad523b905879d43520b68c1be95c51e316c8
Altamash <altamasn186@gmail.com>
2026-06-09 17:15:21 +0530

    02.py

---
+++ 02.py
@@ -0,0 +1,34 @@
+# python keywords
+# there are 35 keywords in python
+# keywords are special reserved words in python for writing code
+
+import keyword as kw
+
+# print(kw.kwlist)
+# print('total keyword in python: ',len(kw.kwlist))
+
+
+# as is pet name for aliasing
+
+# list_of_keyword=kw.kwlist
+# print("hard keyword:\n",list_of_keyword)
+
+# count of hardkeyword
+
+# list_of_keyword=kw.kwlist
+# print("hard keyword:\n",len(list_of_keyword))
+
+
+# for softkeywords
+
+# list_of_keyword=kw.softkwlist
+# print("soft keywords are: ",list_of_keyword)
+
+
+# total=len(list_of_keyword)
+# print(total)
+
+
+
+
+
